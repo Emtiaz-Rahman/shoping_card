561aec59617dd5e0f9ae474c11419ba14bd0db7f
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-02-13 20:11:54 +0600

    case part

--- app.js
+++ app.js
@@ -10,16 +10,37 @@ function updateIphoneNumber(isIncresing) {
     iPhoneInput.value = iPhoneNumber;
     const priceTotal = document.getElementById('price_total');
     priceTotal.innerText = iPhoneNumber * 1219;
-
 }
-
-
 document.getElementById('iPhone_plus').addEventListener('click', function () {
     updateIphoneNumber(true);
-
 })
-
 document.getElementById('iPhone_minus').addEventListener('click', function () {
     updateIphoneNumber(false);
+})
+//    ========================iPhone Part complete
+
+
+function updateCaseNumber(isIncresing) {
+    const caseInput = document.getElementById('case_number');
+    let caseNumber = caseInput.value;
+    if (isIncresing) {
+        caseNumber = parseInt(caseNumber) + 1;
+    }
+    else if (caseNumber > 0) {
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+    const casePriceTotal = document.getElementById('casePrice_total');
+    casePriceTotal.innerText = caseNumber * 59;
+}
+document.getElementById('case_plus').addEventListener('click', function () {
+
+    updateCaseNumber(true);
+})
+document.getElementById('case_minus').addEventListener('click', function () {
+    updateCaseNumber(false);
 
 })
+
+
+
